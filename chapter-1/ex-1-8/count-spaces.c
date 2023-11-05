feba29967aa5161514401688b18a5abb362439d1
jz-ml <138274794+jz-ml@users.noreply.github.com>
2023-11-05 16:19:05 +1100

    Check user input for blanks, tabs and newlines

--- chapter-1/ex-1-8/count-spaces.c
+++ chapter-1/ex-1-8/count-spaces.c
@@ -2,5 +2,20 @@
 
 main()
 {
-    
+    int c, nb, nt, nl;
+
+    nb = 0;
+    nt = 0;
+    nl = 0;
+    while ((c = getchar()) != EOF) {
+        if (c == '\b') {
+            ++nb;
+        }
+        if (c == '\t') {
+            ++nt;
+        }
+        if (c == '\n') {
+            ++nl;
+        }
+    }
 }
