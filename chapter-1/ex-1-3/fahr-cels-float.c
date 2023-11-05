def631c3326c31f76910037f121796f2808f79b8
jz-ml <138274794+jz-ml@users.noreply.github.com>
2023-11-05 14:39:51 +1100

    Print heading above table

--- chapter-1/ex-1-3/fahr-cels-float.c
+++ chapter-1/ex-1-3/fahr-cels-float.c
@@ -9,6 +9,8 @@ main()
     upper = 300;    /* upper limit */
     step = 20;      /* step size */
 
+    printf("Fahrenheit - Celsius\n");
+
     fahr = lower;
     while (fahr <= upper) {
         cels = (5.0 / 9.0) * (fahr - 32.0);
