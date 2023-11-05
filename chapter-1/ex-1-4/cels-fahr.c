35ce367b00be25c36befc116e3e8e64c56447429
jz-ml <138274794+jz-ml@users.noreply.github.com>
2023-11-05 14:54:24 +1100

    Implement cels-fahr conversion

--- chapter-1/ex-1-4/cels-fahr.c
+++ chapter-1/ex-1-4/cels-fahr.c
@@ -2,5 +2,17 @@
 
 main()
 {
-    
+    float cels, fahr;
+    float lower, upper, step;
+
+    lower = 0;      /* lower limit of temperature scale*/
+    upper = 300;    /* upper limit */
+    step = 20;      /* step size */
+
+    cels = lower;
+    while (cels <= upper) {
+        fahr = ((9.0 * cels) / 5) + 32;
+        printf("%3.0f %6.1f\n", cels, fahr);
+        cels = cels + step;
+    }
 }
