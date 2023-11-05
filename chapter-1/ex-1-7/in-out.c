3416f79d1be433d802ac29a2390b5a41c5747875
jz-ml <138274794+jz-ml@users.noreply.github.com>
2023-11-05 15:43:54 +1100

    Remove unnecessary code

--- chapter-1/ex-1-7/in-out.c
+++ chapter-1/ex-1-7/in-out.c
@@ -1,15 +1,10 @@
 #include <stdio.h>
 
-/* printf verifies that the expression(c = getchar()) != EOF is
- * equivalent to 1 within the loop
- */
-
 main()
 {
     int c;
 
     while ((c = getchar()) != EOF) {
-        printf("%d", (c = getchar()) != EOF);
         putchar(c);
     }
 }
