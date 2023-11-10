dbfc731ab49a6977539376eb8bca64d7d1641473
jz-ml <138274794+jz-ml@users.noreply.github.com>
2023-11-10 15:29:36 +1100

    Print literal chars of escape sequences

--- chapter-1/ex-1-10/verbose-escape.c
+++ chapter-1/ex-1-10/verbose-escape.c
@@ -5,6 +5,14 @@ main()
     int c;
 
     while ((c = getchar()) != EOF) {
-        putchar(c);
+        if (c == '\t') {
+            printf("\\t");
+        }
+        if (c == '\b') {
+            printf("\\b");
+        }
+        if (c == '\\') {
+            printf("\\\\");
+        }
     }
 }
