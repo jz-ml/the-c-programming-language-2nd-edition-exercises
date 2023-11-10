edaae658a6cd71117c0ed56269cbeb3756d290d9
jz-ml <138274794+jz-ml@users.noreply.github.com>
2023-11-10 15:38:21 +1100

    Remember if current char is an escape sequence

--- chapter-1/ex-1-10/verbose-escape.c
+++ chapter-1/ex-1-10/verbose-escape.c
@@ -1,18 +1,26 @@
 #include <stdio.h>
 
+#define NON_ESCAPE_SEQ  0
+#define ESCAPE_SEQ      1
+
 main()
 {
-    int c;
+    int c, state;
+
+    state = NON_ESCAPE_SEQ;
 
     while ((c = getchar()) != EOF) {
         if (c == '\t') {
             printf("\\t");
+            state = ESCAPE_SEQ;
         }
         if (c == '\b') {
             printf("\\b");
+            state = ESCAPE_SEQ;
         }
         if (c == '\\') {
             printf("\\\\");
+            state = ESCAPE_SEQ;
         }
     }
 }
