83a79b67725f173dc259d9705a9ad0fda3f78517
jz-ml <138274794+jz-ml@users.noreply.github.com>
2024-02-09 16:42:49 +1100

    Remove putchars and unnecessary comment

--- chapter-1/ex-1-13/word-length-hist.c
+++ chapter-1/ex-1-13/word-length-hist.c
@@ -12,25 +12,15 @@ main()
     while ((c = getchar()) != EOF) {
         if (c == BLANK) {
             if (state == IN) {
-                putchar(c);
+                
             }
             state = OUT;
         }
         else if (c != BLANK) {
-            putchar(c);
+            
             state = IN;
             ++word_length;
         }
     }
 }
 
-/*  If inside a word
- *      Increment word length count for each letter
- *          Print histogram
- *
- * 
- * 
- * 
- * 
- * 
- * */
