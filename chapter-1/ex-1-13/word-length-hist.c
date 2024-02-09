50e2e1c079402ab38e59eb88ab7a8307f9aa7fcf
jz-ml <138274794+jz-ml@users.noreply.github.com>
2024-02-09 12:50:22 +1100

    Ignore contiguous spaces in input

--- chapter-1/ex-1-13/word-length-hist.c
+++ chapter-1/ex-1-13/word-length-hist.c
@@ -1,10 +1,34 @@
 #include <stdio.h>
 
+#define IN      1
+#define OUT     0
+#define BLANK   ' '
+
 main()
 {
-    int c;
+    int c, state;
 
     while ((c = getchar()) != EOF) {
-        putchar(c);
+        if (c == BLANK) {
+            if (state == IN) {
+                putchar(c);
+            }
+            state = OUT;
+        }
+        else if (c != BLANK) {
+            putchar(c);
+            state = IN;
+        }
     }
 }
+
+/*  If inside a word
+ *      Increment word length count for each letter
+ *          Print histogram
+ *
+ * 
+ * 
+ * 
+ * 
+ * 
+ * */
