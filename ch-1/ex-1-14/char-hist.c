56ab13e06923e84ca54930977b99a0d0ee498906
jz-ml <138274794+jz-ml@users.noreply.github.com>
2024-02-14 10:46:07 +1100

    Reset alpha_chars indexes to 0

--- ch-1/ex-1-14/char-hist.c
+++ ch-1/ex-1-14/char-hist.c
@@ -2,8 +2,12 @@
 
 main()
 {
-    int c;
+    int i, c;
     int alpha_chars[26];
+
+    for (i = 0; i < 26; ++i) {
+        alpha_chars[i] = 0;
+    }
 
     while ((c = getchar()) != EOF) {
         putchar(c);
